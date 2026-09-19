769ff7e87e9506a30b9d18febdcc1f7732d0fd4f
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-09-19 16:52:25 +0800

    test: advance automatic POI identity to v9

--- tests/challenge/leaderboard.test.ts
+++ tests/challenge/leaderboard.test.ts
@@ -24,7 +24,7 @@ function submission(score: number, finalTick: number, finalStateHash: string): C
   const challenge = {
     version: BLOCK_CHALLENGE_VERSION,
     blockHeight: 1_000_000,
-    rulesetVersion: 'ef-standard-v8',
+    rulesetVersion: 'ef-standard-v9',
     worldGameplayHash: 'world-hash',
     generationAttempt: 0,
     mode: 'DESTROY' as const,
@@ -38,7 +38,7 @@ function submission(score: number, finalTick: number, finalStateHash: string): C
     challengeCode: 'BC1-TEST0001',
     replay: {
       header: {
-        version: 'ef-replay-v8',
+        version: 'ef-replay-v9',
         blockHeight: challenge.blockHeight,
         rulesetVersion: challenge.rulesetVersion,
         worldGameplayHash: challenge.worldGameplayHash,
