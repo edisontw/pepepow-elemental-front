72e76e25b8066620886d17adf92bb3511792087b
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-09-24 00:48:18 +0800

    update v19 gameplay replay expectations

--- tests/challenge/leaderboard.test.ts
+++ tests/challenge/leaderboard.test.ts
@@ -24,7 +24,7 @@ function submission(score: number, finalTick: number, finalStateHash: string): C
   const challenge = {
     version: BLOCK_CHALLENGE_VERSION,
     blockHeight: 1_000_000,
-    rulesetVersion: 'ef-standard-v18',
+    rulesetVersion: 'ef-standard-v19',
     worldGameplayHash: 'world-hash',
     generationAttempt: 0,
     mode: 'DESTROY' as const,
@@ -38,7 +38,7 @@ function submission(score: number, finalTick: number, finalStateHash: string): C
     challengeCode: 'BC1-TEST0001',
     replay: {
       header: {
-        version: 'ef-replay-v18',
+        version: 'ef-replay-v19',
         blockHeight: challenge.blockHeight,
         rulesetVersion: challenge.rulesetVersion,
         worldGameplayHash: challenge.worldGameplayHash,
